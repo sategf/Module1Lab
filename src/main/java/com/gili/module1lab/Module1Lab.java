09a047d5d342f4137f12e61cd26f527e7f557ce5
Window <Window@DESKTOP-OO3D50H.mshome.net>
2023-01-29 14:21:46 -0500

    1st

---
+++ src/main/java/com/gili/module1lab/Module1Lab.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gili.module1lab;
+
+/**
+ *
+ * @author Gili
+ */
+public class Module1Lab {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
